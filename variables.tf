fa389f7772136cb2af1aaadbcfe94c64cfba9e06
Sonu Kumar <144090304+sonugupta4166@users.noreply.github.com>
2026-04-29 01:31:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0f5ee92e2d63afc18"
+}
+
+variable "bucket_name" {
+  default = "your-unique-project-bucket"
+}
